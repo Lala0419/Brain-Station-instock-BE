6ee7598dcbb7f34a25b32e1a7dd6c55d9f7771ce
Emily W <110256938+emwong101@users.noreply.github.com>
2022-11-23 22:26:48 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.port || 5050;
+const PORT = process.env.PORT || 5050;
 
 const warehouseRoutes = require("./routes/warehouse");
 const inventoryRoutes = require("./routes/inventory");
